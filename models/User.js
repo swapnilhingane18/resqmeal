17153628dd5e2222958d7950fba8a0ebd2938ba1
swapnilhingane18 <swapnilhingane9290@gmail.com>
2026-02-21 21:50:07 +0530

    Added ENERGY_CENTER role support

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['DONOR', 'NGO', 'ADMIN'],
+        enum: ['DONOR', 'NGO', 'ADMIN', 'ENERGY_CENTER'],
         default: 'DONOR'
     },
     location: {
